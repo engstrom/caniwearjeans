69d19c99bde1be0839921cd64ed144fa95c9af57
engstrom <dengstr@wgu.edu>
2014-04-23 21:18:19 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-caniwearjeans
+Can I wear jeans?
 =============
+[caniwearjeans.com](http://caniwearjeans.com/)
+
+A site dedicated to figuring out if I'm allowed to wear jeans to work or not.
---
+++ src/app.js
@@ -0,0 +1,64 @@
+$(document).ready(function jeansDay() {
+  var whitelist = 'data/whitelist.json'
+  var blacklist = 'data/blacklist.json'
+  var currentDate = getCurrentDate();
+  var dayOfWeek = new Date().getDay();
+  var answer = '';
+  
+  if (dayOfWeek === 5 && !onList(blacklist, currentDate)) {
+    answer = 'YES!';
+  } else if (onList(whitelist, currentDate)) {
+    answer = 'YES!';
+  } else {
+    answer = 'NO';
+  }
+
+  $("p").text(answer);
+});
+
+function onList(listPath, currentDate) {
+  var json = getJsonData(listPath);
+  var result = false;
+  
+  $.each(json, function(i, item) {
+    if (json[i].date === currentDate) {
+      result = true;
+    }
+  });
+
+  return result;
+}
+
+function getJsonData(path) {
+  var json = [];
+
+  $.ajax({
+    url: path,
+    async: false,
+    dataType: 'json',
+    success: function (response) {
+      json = response;
+    }
+  });
+
+  return json;
+}
+
+function getCurrentDate() {
+  // This function comes from http://stackoverflow.com/a/4929629/1451531
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+    dd='0'+dd
+  } 
+
+  if(mm<10) {
+    mm='0'+mm
+  } 
+
+  today = mm+'/'+dd+'/'+yyyy;
+  return today;
+}
